4f15a4dfa59177614634242ca91d2e5d5dbe6f4a
Andy Loftus <aloftus@gmail.com>
2020-01-16 13:27:45 -0600

    Initial commit

---
+++ manifests/allow_from.pp
@@ -0,0 +1,183 @@
+# Enable incoming ssh connections for a given set of hosts
+#
+# @summary Enable incoming ssh for a given set of hosts
+#   + Update iptables firewall
+#   + Update sshd_config with a Match directive and associated settings
+#   + Update access.conf
+#   + Update tcpwrappers
+#
+# @param users
+#   Type: Array
+#   Desc: list of users to allow (from hostlist)
+#   Note: If both "users" and "groups" are empty, error is raised.
+#
+# @param groups
+#   Type: Array
+#   Desc: list of groups to allow (from hostlist)
+#   Note: If both "users" and "groups" are empty, error is raised.
+#
+# @param hostlist
+#   Type: Array
+#   Desc: list of IPs or Hostnames that (users/groups) are allowed to ssh from
+#
+# @param additional_match_params
+#   Type: Hash
+#   Desc: sshd config keywords and values
+#   Format: additional_match_params = { 'keyword1' => 'value1',
+#                                       'keyword2' => 'value2',
+#                                       'keyword3' => [ 'val3_1','val3_2' ],
+#                                     }
+#
+# @example
+#   sshd::allow_from { 'namevar': }
+#
+# @example
+#   ssh::allow_from { 'allow incoming ssh by users 1,2,3 from hosts X,Y,Z':
+#       'users'                   => Array,
+#       'groups'                  => Array,
+#       'hostlist'                => Array,
+#       'additional_match_params' => Hash,
+#   }
+define sshd::allow_from (
+    Array[ String, 1 ]   $hostlist,
+    Array[ String ]      $users,
+    Array[ String ]      $groups,
+    Hash[ String, Data ] $additional_match_params,
+) {
+
+    ### CHECK INPUT
+    if empty( $users ) and empty( $groups ) {
+        fail( "'users' and 'groups' cannot both be empty" )
+    }
+
+
+    ### ACCESS.CONF
+    ### This sets up the pam access.conf file to allow incoming ssh
+    $groups.each |String $group| { $hostlist.each |String $host| {
+        pam_access::entry { "Allow ${group} ssh from ${host}":
+            group      => $group,
+            origin     => $host,
+            permission => '+',
+            position   => '-1',
+        }
+    }}
+    $users.each |String $user| { $hostlist.each |String $host| {
+        pam_access::entry { "Allow ${user} ssh from ${host}":
+            user       => $user,
+            origin     => $host,
+            permission => '+',
+            position   => '-1',
+        }
+    }}
+
+
+    ### FIREWALL
+    $hostlist.each | $host | {
+        firewall { "222 allow SSH from ${host} for ${name}":
+            dport  => 22,
+            proto  => tcp,
+            source => $host,
+            action => accept,
+        }
+    }
+
+
+    ### TCPWRAPPERS
+    $hostlist.each | $host | {
+        tcpwrappers::allow { "tcpwrappers allow SSH from host '${host}'":
+            service => 'sshd',
+            address => $host,
+        }
+    }
+
+
+    ### SSSD
+    # Requires custom fact 'sssd_domains'
+    # See: lib/augeasfacter/sssd_info.conf
+    # See also: https://github.com/woodsbw/augeasfacter
+    ###
+    # convert sssd domains from csv string to a puppet array
+    $domains = $facts['sssd_domains'].regsubst(/ +/, '', 'G').split(',')
+    $domains.each |$domain| {
+        if $users =~ Array[String,1] {
+            $csv = $users.join(',')
+            ::sssd::domain::array_append { "${name} users '${csv}' for sssd domain '${domain}'" :
+                domain  => $domain,
+                setting => 'simple_allow_users',
+                items   => $users,
+            }
+        }
+        if $groups =~ Array[String,1] {
+            $csv = $groups.join(',')
+            ::sssd::domain::array_append { "${name} groups '${csv}' for sssd domain '${domain}'" :
+                domain  => $domain,
+                setting => 'simple_allow_groups',
+                items   => $groups,
+            }
+        }
+    }
+
+    ### SSHD_CONFIG
+    # Defaults
+    $config_defaults = {
+        'notify' => Service[ sshd ],
+    }
+    $config_match_defaults = $config_defaults + {
+        'position' => 'before first match'
+    }
+
+    # Create cfg_match_params for Users and Groups
+    $user_params = $users ? {
+        Array[ String, 1 ] => { 'AllowUsers' => $users },
+        default            => {}
+    }
+    $group_params = $groups ? {
+        Array[ String, 1 ] => { 'AllowGroups' => $groups },
+        default            => {}
+    }
+
+    # Combine all cfg_match_params into a single hash
+    $cfg_match_params = $additional_match_params + $user_params + $group_params
+
+    # Hostnames require "Match Host"
+    # IPs/CIDRs require "Match Address"
+    # Create separate lists and make two separate match blocks in sshd_config
+    # Criteria will be either "Host" or "Address"
+    # Pattern will be the CSV string of hostnames or IPs
+    # See also: "sshd_config" man page, for details of "criteria-pattern pairs"
+    $name_list = $hostlist.filter | $elem | { $elem =~ /[a-zA-Z]/ }
+    $ip_list   = $hostlist.filter | $elem | { $elem !~ /[a-zA-Z]/ }
+    #associate the correct criteria with each list, filter empty lists
+    $host_data = {
+        'Host'    => $name_list,
+        'Address' => $ip_list,
+    }.filter | $criteria, $list | {
+        size( $list ) > 0
+    }
+    #loop through host_data creating a match block for each criteria-pattern
+    $host_data.each | $criteria, $list | {
+        $pattern = join( $list, ',' )
+        $match_condition = "${criteria} ${pattern}"
+
+        #create match block
+        sshd_config_match {
+            $match_condition :
+            ;
+            default: * => $config_match_defaults,
+            ;
+        }
+
+        #add parameters to the match block
+        $cfg_match_params.each | $key, $val | {
+            sshd_config {
+                "${match_condition} ${key}" :
+                    key       => $key,
+                    value     => $val,
+                    condition => $match_condition,
+                ;
+                default: * => $config_defaults,
+                ;
+            }
+        }
+    }
+}
---
+++ manifests/init.pp
@@ -0,0 +1,133 @@
+# @summary Configure default sshd settings
+#
+# @param trusted_subnets
+#   Array of IPs and CIDRs to be allowed through the firewall
+#   Values from multiple sources are merged
+#
+# @param config
+#   Hash of global config settings
+#   Defaults provided by this module
+#   Values from multiple sources are merged
+#   Key collisions are resolved in favor of the higher priority value
+#
+# @param config_matches
+#   Hash of config "match" conditions and settings
+#   Keys are match condition
+#   Vals are a hash of sshd_config settings for the match condition
+#   Expected format:
+#   ---
+#   sshd::config_matches:
+#     Unique condition one:
+#       SettingOne: val
+#       SettingTwo:
+#         - val2
+#         - val3
+#     Unique condition two:
+#       SettingOne: val
+#       SettingTwo:
+#         - val2
+#         - val3
+#   Note that condition strings must be valid sshd_config criteria-pattern pairs
+#   Values from multiple sources are merged
+#   Key collisions are resolved in favor of the higher priority value
+#   Merges are deep to allow use of the knockout_prefix '-' (to remove a key
+#   from the final result).
+#
+# @param revoked_keys
+#   List of ssh public keys to disallow
+#   Values from multiple sources are merged
+#
+# @note Parameters below here are either OS dependent or not likely to change
+# often
+#
+# @param required_packages
+#   List of package names to be installed (OS specific)
+#   (Defaults provided by module should be sufficient)
+#
+# @example
+#   include sshd
+class sshd (
+    Array             $trusted_subnets,
+    Hash              $config,
+    Hash[String,Hash] $config_matches,
+    Array[String]     $revoked_keys,
+
+    # Module defaults should be sufficient
+    Array[String] $required_packages,   #per OS
+    String        $revoked_keys_file,   #per OS
+) {
+
+    # PACKAGES
+    ensure_packages( $required_packages, {'ensure' => 'present'} )
+
+    # SERVICE
+    service { 'sshd' :
+        ensure     => running,
+        enable     => true,
+        hasstatus  => true,
+        hasrestart => true,
+        require    => Package[ $required_packages ],
+    }
+
+    # FIREWALL
+    each($trusted_subnets) | $index, $sshd_subnet | {
+        firewall { "022 allow SSH from ${sshd_subnet}":
+            dport  => 22,
+            proto  => tcp,
+            source => $sshd_subnet,
+            action => accept,
+        }
+    }
+
+    # REVOKED KEYS
+    file { $revoked_keys_file :
+        ensure  => present,
+        owner   => root,
+        group   => root,
+        mode    => '0644',
+        content => join( $revoked_keys, "\n" ),
+        notify  => Service['sshd'],
+    }
+
+    # SSHD CONFIG SETTINGS
+
+    # Default sshd_config attributes
+    $config_defaults = {
+        'notify' => Service[ sshd ],
+    }
+    $config_match_defaults = $config_defaults + { 'position' => 'before first match' }
+
+    # Apply global sshd_config settings
+    $config.each | $key, $val | {
+        sshd_config {
+            $key : value => $val,
+            ;
+            default: * => $config_defaults,
+            ;
+        }
+    }
+
+    # Process config match settings from Hiera
+    $config_matches.each | $condition, $data | {
+        # Create config match section
+        sshd_config_match {
+            $condition :
+            ;
+            default: * => $config_match_defaults,
+            ;
+        }
+        # Set each setting inside the match section
+        $data.each | $key, $val | {
+            sshd_config {
+                "${condition} ${key}" :
+                    key       => $key,
+                    value     => $val,
+                    condition => $condition,
+                ;
+                default: * => $config_defaults,
+                ;
+            }
+        }
+    }
+
+}
